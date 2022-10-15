c115ec24d88a0d9b6fec0091d80230203ab07208
Kasope Johnson <kasopej@gmail.com>
2022-10-15 22:35:12 +0100

    adds image, removes some boiler-plate

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <main>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar toggleable="lg" type="dark">
+      <b-navbar-brand href="#" class="text-primary">NavBar</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item to="home" class="pad-nav">Link</b-nav-item>
+          <b-nav-item to="home" class="pad-nav">Home</b-nav-item>
           <b-nav-item href="#" class="pad-nav">Medical Service</b-nav-item>
           <b-nav-item href="#" class="pad-nav">Login</b-nav-item>
           <b-nav-item href="#" class="btn btn-primary pad-nav"
@@ -16,12 +16,6 @@
     </b-navbar>
   </main>
   <router-view />
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">Title</h5>
-      <p class="card-text">Content</p>
-    </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -31,6 +25,17 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+main {
+  background-image: url('@/assets/hospital-healthcare-workers-concept-young-doctor.png');
+  background-size: cover;
+  background-position: center center;
+}
+
+.navbar-brand {
+  font-family: hanson;
+  font-size: 25px;
 }
 .pad-nav {
   margin-right: 40px;
